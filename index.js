632e9b31498c2340a3b87ef329fff00c6b5e16b4
paul <pxl34232@gmail.com>
2020-12-08 16:15:17 -0500

    started index

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("inside demo8dec index.js");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const server = express();
+
+server.use(bodyParser.json());
+
+//server.listen(3000);
+server.listen(process.env.PORT || 3000);
